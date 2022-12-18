93e32a0b33c42183052af87e6c7f00356d8168d7
Carlos Molmelstet <carlos.molmelstet@credpago.com>
2022-12-18 20:56:22 -0300

    ajuste no build

--- src/services/leagueOfLegendsClient.ts
+++ src/services/leagueOfLegendsClient.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export function getLeagueOfLegendsClient(ctx?: any) {
+export function getLeagueOfLegendsClient() {
   const api = axios.create({
     baseURL: 'https://ddragon.leagueoflegends.com/cdn/12.23.1/data/pt_BR/',
   });
--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,4 +1,3 @@
-
 import Alert from './Alert';
 import Button from './Button';
 import FormLabel from './FormLabel';
